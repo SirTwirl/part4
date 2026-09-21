3d0c10a89ad744cd918e4ef1c2f71a447401cc72
SirTwirl <j.kruk123@o2.pl>
2026-09-21 12:26:17 +0200

    test(backend): add totalLikes helper function and unit tests

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const sumLikes = blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0)
+    return sumLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
