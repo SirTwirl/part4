d63d21634badb886475126bd21496bc3a77953e3
SirTwirl <j.kruk123@o2.pl>
2026-09-21 12:48:46 +0200

    test(bloglist): add favoriteBlog helper function and unit tests

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -7,7 +7,15 @@ const totalLikes = (blogs) => {
     return sumLikes
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+    return blogs.reduce((max, blog) => {
+        return blog.likes > max.likes ? blog : max
+    })
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
