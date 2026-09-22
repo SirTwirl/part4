8526d5ced51960578c117dcbb52c349aecd98635
SirTwirl <j.kruk123@o2.pl>
2026-09-22 19:47:08 +0200

    refactor(test_helper): remove unused helper functions

--- bloglist/tests/test_helper.js
+++ bloglist/tests/test_helper.js
@@ -24,26 +24,13 @@ const initialUsers = [
   }
 ]
 
-const nonExistingId = async () => {
-  const blog = new Blog({ title: 'willremovethissoon', url: 'https://example.com' })
-  await blog.save()
-  await blog.deleteOne()
-
-  return blog._id.toString()
-}
-
-const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
-}
-
 const usersInDb = async () => {
   const users = await User.find({})
   return users.map(u => u.toJSON())
 }
 
 module.exports = {
-  initialBlogs,
-  initialUsers,
-  usersInDb
+    initialBlogs,
+    initialUsers,
+    usersInDb
 }
