a7b0875b954f9ec6af09c3ec1b0bb1a6c4c18ed5
SirTwirl <j.kruk123@o2.pl>
2026-09-22 18:22:36 +0200

    feat(middleware): add tokenExtractor middleware

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -3,14 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 const config = require('../utils/config')
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
+const getTokenFrom = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -18,13 +11,8 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const token = getTokenFrom(request)
-  
-  if (!token) {
-    return response.status(401).json({ error: 'token missing' })
-  }
+  const decodedToken = getTokenFrom(request.token, config.SECRET)
 
-  const decodedToken = jwt.verify(token, config.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' })
   }
